45d7c75c1cbbc1a0ca2f0ca82ab43bec6ac95af3
brleinad <daniel.rodas.bautista@gmail.com>
2020-04-22 21:02:17 -0400

    Updated sign up form and template so its uses bulma and looks a bit nicer

--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,24 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].help_text = '''
+Your password can’t be too similar to your other personal information.
+Your password must contain at least 8 characters.
+Your password can’t be a commonly used password.
+Your password can’t be entirely numeric.
+'''
+
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('username', 'email',)
+        fields = ('username', 'email', 'password1', 'password2',)
+        #help_texts['password1'] = 'BOB'
+        help_texts = {
+                'password1': 'BOB',
+                }
 
 
 class CustomUserChangeForm(UserChangeForm):
