aa973cffc68a7c6b6a06c84bd659c3c3b5ab0f28
brleinad <daniel.rodas.bautista@gmail.com>
2020-05-10 15:28:23 -0400

    More updates for heroku deployment

--- upweekly_project/settings.py
+++ upweekly_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional devault value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
