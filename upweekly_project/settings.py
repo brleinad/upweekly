80c133080229314d8d8e69a7e06a428a9928891c
brleinad <daniel.rodas.bautista@gmail.com>
2020-05-10 15:32:22 -0400

    fixed typo

--- upweekly_project/settings.py
+++ upweekly_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'Optional devault value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
